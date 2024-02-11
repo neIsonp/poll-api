b815efb34f096a1709f57459e421e07f2fd5dfe2
neIsonp <nelsonponte37@gmail.com>
2024-02-11 21:49:41 +0100

    add create poll route

---
+++ src/http/routes/create-pool.ts
@@ -0,0 +1,29 @@
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify";
+
+export async function createPool(app: FastifyInstance) {
+  app.post("/pools", async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string(),
+      options: z.array(z.string()),
+    });
+
+    const { title, options } = createPollBody.parse(request.body);
+
+    const pool = await prisma.poll.create({
+      data: {
+        title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return { title: option };
+            }),
+          },
+        },
+      },
+    });
+
+    return reply.status(201).send({ poolId: pool.id });
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,31 +1,15 @@
-import {  PrismaClient } from "@prisma/client"
-import fastify from "fastify"
-import { request } from "http"
-import { title } from "process"
-import {z} from "zod"
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { request } from "http";
+import { title } from "process";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { createPool } from "./routes/create-pool";
 
-const app = fastify()
+const app = fastify();
 
-const prisma = new PrismaClient()
+app.register(createPool);
 
-app.post('/pools', async (request, reply) =>{
-
-  const createPollBody = z.object({
-    title: z.string()
-  })
-
-  const {title} = createPollBody.parse(request.body)
-
-  const pool = await prisma.pool.create({
-    data: {
-      title
-    }
-  })
-
-  return reply.status(201).send({ poolId: pool.id})
-})
-
-
-app.listen({port: 3333}).then(() =>{
-  console.log("Server running")
-})
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running");
+});
